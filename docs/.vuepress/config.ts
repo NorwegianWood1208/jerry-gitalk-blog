a19cb146c1c209b7e16cd21b1f43af77ac1f8e5f
wujy@fapiao.com <18500892223@163.com>
2022-03-09 14:17:08 +0800

    提交新文件

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,294 @@
+import { resolve } from 'path'
+import { defineConfig4CustomTheme } from 'vuepress/config'
+import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types'
+import dayjs from 'dayjs'
+import baiduCode from './config/baiduCode' // 百度统计hm码
+import htmlModules from './config/htmlModules' // 自定义插入的html块
+
+
+export default defineConfig4CustomTheme<VdoingThemeConfig>({
+  theme: 'vdoing', // 使用npm包主题
+  // theme: resolve(__dirname, '../../vdoing'), // 使用本地主题
+
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: "Jerry's blog",
+      description: '个人博客,分享自己对于这个世界一些不客观的观点。',
+    }
+  },
+  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+
+  // 主题配置
+  themeConfig: {
+    // 导航配置
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '读书',
+        link: '/read/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        items: [
+          { text: '2022年2月', link: '/read/fdba22/' },
+          { text: '《沧浪之水》', link: '/read/a61298/' },
+          { text: '《置身事内》', link: '/pages/bd7714/' },
+        ],
+      },
+      {
+        text: '生活',
+        link: '/life/',
+        items: [
+          { text: '记在2021年底', link: '/life/06f7e2/' },
+        ],
+      },
+      {
+        text: '技术',
+        link: '/technology/',
+        items: [
+          { text: '快速配置前端的Macbook', link: '/pages/0c8a93/' },
+          { text: 'vite核心原理及在vue2项目中的使用', link: '/pages/811b36/' },
+        ],
+      },
+      {
+        text: '更多',
+        link: '/more/',
+        items: [
+          { text: '足球',
+            items: [
+              { text: '《记在2021年法国队淘汰后》', link: '/pages/4c17d5/' },
+              { text: '我们为什么喜欢足球', link: '/pages/96be87/' },
+            ],
+          },
+        ],
+      },
+      { text: '关于', link: '/about/' },
+      {
+        text: '收藏',
+        link: '/pages/beb6c0bd8a66cea6/',
+        // items: [
+        //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+        //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+        //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+        // ],
+      },
+      {
+        text: '索引',
+        link: '/archives/',
+        items: [
+          { text: '分类', link: '/categories/' },
+          { text: '标签', link: '/tags/' },
+          { text: '归档', link: '/archives/' },
+        ],
+      },
+    ],
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    logo: '/img/j_by_climei.png', // 导航栏logo
+    repo: '', // 导航栏右侧生成Github链接
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
+    lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
+    docsDir: 'docs', // 编辑的文件夹
+    editLinks: true, // 启用编辑
+    editLinkText: '编辑',
+
+    //*** 以下是Vdoing主题相关配置，文档：https://doc.xugaoyi.com/pages/a20ce8/ ***//
+
+    // category: false, // 是否打开分类功能，默认true
+    // tag: false, // 是否打开标签功能，默认true
+    // archive: false, // 是否打开归档功能，默认true
+    // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
+
+    bodyBgImg: [
+      'https://cdn.jsdelivr.net/gh/NorwegianWood1208/jerry-gitalk-blog@main/WechatIMG5225.jpeg',
+      'https://cdn.jsdelivr.net/gh/NorwegianWood1208/jerry-gitalk-blog@main/WechatIMG5226.jpeg',
+      'https://cdn.jsdelivr.net/gh/NorwegianWood1208/jerry-gitalk-blog@main/WechatIMG5227.jpeg'
+    ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时每隔15秒换一张。
+    // bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0.1~ 1.0, 默认0.5
+    // titleBadge: false, // 文章标题前的图标是否显示，默认true
+    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
+    //   '图标地址1',
+    //   '图标地址2'
+    // ],
+    // contentBgStyle: 1, // 文章内容块的背景风格，默认无. 1 方格 | 2 横线 | 3 竖线 | 4 左斜线 | 5 右斜线 | 6 点状
+
+    // updateBar: { // 最近更新栏
+    //   showToArticle: false, // 显示到文章页底部，默认true
+    //   moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+    // },
+    // rightMenuBar: false, // 是否显示右侧文章大纲栏，默认true (屏宽小于1300px下无论如何都不显示)
+    // sidebarOpen: false, // 初始状态是否打开左侧边栏，默认true
+    // pageButton: false, // 是否显示快捷翻页按钮，默认true
+
+    // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    sidebar: 'structuring',
+
+    // 文章默认的作者信息，(可在md文件中单独配置此信息) string | {name: string, link?: string}
+    author: {
+      name: 'Jerry', // 必需
+      link: 'https://github.com/NorwegianWood1208', // 可选的
+    },
+
+    // 博主信息 (显示在首页侧边栏)
+    blogger: {
+      avatar: 'https://img1.baidu.com/it/u=3110618223,3718234292&fm=253&fmt=auto&app=138&f=JPG?w=500&h=694',
+      name: 'Jerry',
+      slogan: 'Kop、摇滚迷、web前端',
+    },
+
+    // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
+    social: {
+      // iconfontCssFile: '//at.alicdn.com/t/xxx.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加。阿里图片库：https://www.iconfont.cn/
+      icons: [
+        {
+          iconClass: 'icon-youjian',
+          title: '发邮件',
+          link: '18500892223@163.com',
+        },
+        {
+          iconClass: 'icon-github',
+          title: 'GitHub',
+          link: 'https://github.com/NorwegianWood1208',
+        },
+        {
+          iconClass: 'icon-erji',
+          title: '听音乐',
+          link: '',
+        },
+      ],
+    },
+
+    // 页脚信息
+    footer: {
+      createYear: 2022, // 博客创建年份
+      copyrightInfo:
+        'Jerry', // 博客版权信息，支持a标签或换行标签</br>
+    },
+
+    // 自定义hmtl(广告)模块
+    htmlModules
+  },
+
+  // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+  head: [
+    ['link', { rel: 'icon', href: '/img/j_by_climei.png' }], //favicons，资源放在public文件夹
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
+      },
+    ],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    // [
+    //   'script',
+    //   {
+    //     'data-ad-client': 'ca-pub-7828333725993554',
+    //     async: 'async',
+    //     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+    //   },
+    // ], // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
+  ],
+
+  // 插件配置
+  plugins: {
+    // 导入本地插件（供学习参考）
+    // [resolve(__dirname, './plugins/love-me')]: { // 鼠标点击爱心特效
+    //   color: '#11a8cd', // 爱心颜色，默认随机色
+    //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
+    // },
+
+    // 百度自动推送
+    'vuepress-plugin-baidu-autopush': {},
+
+    // 百度统计
+    'vuepress-plugin-baidu-tongji': {
+      hm: baiduCode,
+    },
+
+    // 全文搜索
+    'fulltext-search': {},
+
+    // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
+    // 'thirdparty-search': {
+    //   thirdparty: [
+    //     {
+    //       title: '在MDN中搜索',
+    //       frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+    //       behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+    //     },
+    //     {
+    //       title: '在Runoob中搜索',
+    //       frontUrl: 'https://www.runoob.com/?s=',
+    //     },
+    //     {
+    //       title: '在Vue API中搜索',
+    //       frontUrl: 'https://cn.vuejs.org/v2/api/#',
+    //     },
+    //     {
+    //       title: '在Bing中搜索',
+    //       frontUrl: 'https://cn.bing.com/search?q=',
+    //     },
+    //     {
+    //       title: '通过百度搜索本站的',
+    //       frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
+    //     },
+    //   ],
+    // },
+
+    // 代码块复制按钮
+    'one-click-copy': {
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
+      showInMobile: false, // whether to display on the mobile side, default: false.
+    },
+
+    // DEMO演示模块, API: https://github.com/xiguaxigua/vuepress-plugin-demo-block
+    'demo-block': {
+      settings: {
+        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+        // cssLib: ['http://xxx'], // 在线示例中的css依赖
+        // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+        jsfiddle: false, // 是否显示 jsfiddle 链接
+        codepen: true, // 是否显示 codepen 链接
+        horizontal: false, // 是否展示为横向样式
+      },
+    },
+
+    // 放大图片
+    'vuepress-plugin-zooming': {
+      selector: '.theme-vdoing-content img:not(.no-zoom)', // not排除class是no-zoom的图片
+      options: {
+        bgColor: 'rgba(0,0,0,0.6)',
+      },
+    },
+
+    // 评论区
+    'vuepress-plugin-comment': {
+      choosen: 'gitalk',
+      options: {
+        clientID: '7cf83bf1409eb3c4d756',
+        clientSecret: 'c572aace572cf7c4e95bba084929cf2677270c61',
+        repo: 'jerry-gitalk-blog', // GitHub 仓库
+        owner: 'NorwegianWood1208', // GitHub仓库所有者
+        admin: ['NorwegianWood1208'], // 对仓库有写权限的人
+        // distractionFreeMode: true,
+        pagerDirection: 'last', // 'first'正序 | 'last'倒序
+        id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
+        title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
+        labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+        body:
+          '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+      },
+    },
+
+    // "上次更新"的时间格式
+    '@vuepress/last-updated': {
+      transformer: (timestamp, lang) => {
+        return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+      },
+    },
+  },
+
+  markdown: {
+    lineNumbers: true
+  }
+})
